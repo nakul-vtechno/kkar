f1e945a2ff490f94ca47d9312007dbb3414ab33c
Nakul Sharma <34645422+nakul-vtechno@users.noreply.github.com>
2024-02-29 20:24:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://kkar-production.up.railway.app:3000"
 };
 
 app.use(cors(corsOptions));
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
